d3b673560c9e723148598d9f2992b684b29c38b2
John <john@solarhot.org>
2021-06-08 11:32:40 +1000

    periodic

--- AceBMS.ino
+++ AceBMS.ino
@@ -30,10 +30,10 @@ typedef struct {
 
 static bms_t bms;
 
-void writeFrame(tinframe_t* frame){
-  frame->data[MSG_SEQ_OFFSET] = frameSequence;
-  aceBus.write(frame);
-}
+// void writeFrame(tinframe_t* frame){
+//   frame->data[MSG_SEQ_OFFSET] = frameSequence;
+//   aceBus.write(frame);
+// }
 
 void process(void) {
   // called 4 times per second, triggered by current shunt canbus message
@@ -79,9 +79,12 @@ void process(void) {
   msg_data_t *msg = (msg_data_t *)&txFrame.data[MSG_ID_OFFSET];
   msg_pack(msg, BMS_VBAT, cellSum);  // send battery voltage and current
   msg_pack(msg, BMS_IBAT, (int16_t)(bms.chargeMilliAmps / 10));
+
   msg_pack(msg, BMS_VTRG, 26700);
   msg_pack(msg, BMS_ITRG, 2000);
-  writeFrame(&txFrame);
+
+  txFrame.data[MSG_SEQ_OFFSET] = ++frameSequence;
+  aceBus.write(&txFrame);
   heartBeat = true;
 }
 
@@ -102,44 +105,46 @@ void setup() {
   mcp2515.setNormalMode();
 }
 
-#define SCHEDMSK (0xFE)
+// #define SCHEDMSK (0xFE)
 
 void loop() {
   // update bus
   int status = aceBus.update();
   if(status == AceBus_kWriteComplete){
-    frameSequence++;
     // send other parameters when sequence number matches message ID
     if(heartBeat){
       heartBeat = false;
       tinframe_t txFrame;
+      txFrame.data[MSG_SEQ_OFFSET] = frameSequence;
+      txFrame.data[MSG_ID_OFFSET] = frameSequence;
       msg_data_t *msg = (msg_data_t *)&txFrame.data[MSG_ID_OFFSET];
-      if((frameSequence & SCHEDMSK) == (MSG_ID(BMS_CEL1) & SCHEDMSK)){
+      if(frameSequence == MSG_ID(BMS_CEL1)){
         msg_pack(msg, BMS_CEL1, bms.cellVoltage[0]);
         msg_pack(msg, BMS_CEL2, bms.cellVoltage[1]);
         msg_pack(msg, BMS_CEL3, bms.cellVoltage[2]);
         msg_pack(msg, BMS_CEL4, bms.cellVoltage[3]);
-        writeFrame(&txFrame);
-      } else if((frameSequence & SCHEDMSK) == (MSG_ID(BMS_CEL5) & SCHEDMSK)){
+        aceBus.write(&txFrame);
+      } else if(frameSequence == MSG_ID(BMS_CEL5)){
         msg_pack(msg, BMS_CEL5, bms.cellVoltage[4]);
         msg_pack(msg, BMS_CEL6, bms.cellVoltage[5]);
         msg_pack(msg, BMS_CEL7, bms.cellVoltage[6]);
         msg_pack(msg, BMS_CEL8, bms.cellVoltage[7]);
-        writeFrame(&txFrame);
-      } else if((frameSequence & SCHEDMSK) == (MSG_ID(BMS_VBAL) & SCHEDMSK)){
+        aceBus.write(&txFrame);
+      } else if(frameSequence == MSG_ID(BMS_VBAL)){
         msg_pack(msg, BMS_VBAL, bms.balanceVoltage);
         msg_pack(msg, BMS_CHAH, bms.cellVoltage[1]);
         msg_pack(msg, BMS_BTC1, bms.temperature[0]);
         msg_pack(msg, BMS_BTC2, bms.temperature[1]);
-        writeFrame(&txFrame);
+        aceBus.write(&txFrame);
       }
     }
   } else if(status == AceBus_kReadDataReady){
     tinframe_t rxFrame;
-    int status = aceBus.read(&rxFrame);
-    if(status == AceBus_kOK){
-      frameSequence = rxFrame.data[MSG_SEQ_OFFSET] + 1;
-    }
+    // int status =
+    aceBus.read(&rxFrame);  // dummy read to empty rx buffer
+    // if(status == AceBus_kOK){
+    //   frameSequence = rxFrame.data[MSG_SEQ_OFFSET] + 1;
+    // }
   }
 
   if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK) {
